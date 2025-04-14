766489f518d9c103ebe3f8ef2dfbd72225043b92
shaibi62 <146065158+shaibi62@users.noreply.github.com>
2025-04-14 21:38:05 +0500

    nav-update

--- src/components/layout/Navbar.jsx
+++ src/components/layout/Navbar.jsx
@@ -1,12 +1,25 @@
 import { useState } from 'react';
 import logo from '../../assets/logo-bg.png';
 import './Navbar.css'; 
+import { Link, useNavigate } from 'react-router-dom';
+import { useAuth } from '../../hooks/useAuth';
 
 
 export default function Header() {
 const [menuOpen, setMenuOpen] = useState(false);
 const classNav = "cursor-pointer mr-5 sm:px-6 py-3 w-1/2 sm:w-auto sm:justify-start border-b-2 border-[#3B82F6] title-font font-medium bg-transparent inline-flex items-center leading-none text-[#3B82F6] tracking-wider rounded-t hover:text-[#6366F1] hover:bg-[#F3F4F6] hover:border-[#6366F1]";
+
+ const { isAuthenticated, user, logout, isLoading } = useAuth();
+  const navigate = useNavigate();
+
+  const handleLogout = async () => {
+    await logout();
+    navigate('/login'); // Redirect to login after logout
+  };
+
+
 return (
+
 <>
 <header className="bg-white">
       <div className="container flex flex-wrap p-5 flex-col md:flex-row items-center justify-start">
@@ -44,7 +57,7 @@ return (
                  stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"
                  className="w-5 h-5 mr-3">
               <path d="M3 9.5L12 3l9 6.5V20a1 1 0 0 1-1 1h-5v-6H9v6H4a1 1 0 0 1-1-1V9.5z" />
-            </svg> HOME
+            </svg> <Link to={"/"}>HOME</Link>
           </a>
 
           {/* TEMPLATES */}
@@ -55,7 +68,7 @@ return (
               <rect x="3" y="4" width="18" height="16" rx="2" ry="2" />
               <line x1="3" y1="10" x2="21" y2="10" />
               <line x1="8" y1="4" x2="8" y2="10" />
-            </svg> TEMPLATES
+            </svg> <Link to="/templates">TEMPLATES</Link>
           </a>
 
           {/* SIGNUP */}
@@ -67,7 +80,7 @@ return (
               <circle cx="8.5" cy="7" r="4" />
               <line x1="20" y1="8" x2="20" y2="14" />
               <line x1="17" y1="11" x2="23" y2="11" />
-            </svg> SIGNUP
+            </svg> <Link to={"/Signup"}>SIGNUP</Link>
           </a>
 
           {/* LOGIN */}
@@ -78,7 +91,7 @@ return (
               <path d="M15 3h4a2 2 0 0 1 2 2v14a2 2 0 0 1-2 2h-4" />
               <polyline points="10 17 15 12 10 7" />
               <line x1="15" y1="12" x2="3" y2="12" />
-            </svg> LOGIN
+            </svg> <Link to={"/Login"}>LOGIN</Link>
           </a>
 
         </nav>
