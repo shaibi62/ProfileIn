4f8996023c062f5c86a113ba429bf0c0a2449eb2
shaibi62 <146065158+shaibi62@users.noreply.github.com>
2025-04-21 20:03:42 +0500

    s

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { useNavigate, useLocation } from 'react-router-dom';
+import { Link, useNavigate, useLocation } from 'react-router-dom';
 import { useAuth } from '../hooks/useAuth';
 import './PageStyles.css'; // Basic styling for forms
 
