4611a807232ba99a0ab6f525c4742460d11465d4
shaibi62 <146065158+shaibi62@users.noreply.github.com>
2025-04-21 13:17:26 +0500

    Update LoginPage

--- src/pages/LoginPage.jsx
+++ src/pages/LoginPage.jsx
@@ -79,8 +79,8 @@ const Login = () => {
         </center>
         <p className="mt-4 text-center text-sm text-gray-500">
           Don’t have an account?{' '}
-          <a href="/signup" className="text-sky-600 hover:underline">
-            Sign up
+          <a className="text-sky-600 hover:underline">
+            <Link to={"/signup"}>Sign up</Link> 
           </a>
         </p>
       </div>
