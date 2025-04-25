a03f8869c429eabe5d0e62f67c9ae4e6134fd5ce
Zeeshan-Tarar <zeeshantarar3184@gmail.com>
2025-04-25 11:52:12 +0500

    contact

--- src/pages/ContactPage.jsx
+++ src/pages/ContactPage.jsx
@@ -10,7 +10,7 @@ import {
 export default function Contact()
 {
     return(
-      <div className="py-20">
+      <div className="py-20 bg-gray-100">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="text-center mb-16">
           <h1 className="text-4xl font-bold text-gray-900 mb-4">Contact Us</h1>
