521e3d81496ec9970d226d7fa81fe176cd9f972d
shaibi62 <146065158+shaibi62@users.noreply.github.com>
2025-05-20 10:41:09 +0500

    dbprofilein-added

---
+++ dbprofilein.sql
@@ -0,0 +1,378 @@
+-- phpMyAdmin SQL Dump
+-- version 5.2.1
+-- https://www.phpmyadmin.net/
+--
+-- Host: 127.0.0.1
+-- Generation Time: May 20, 2025 at 07:27 AM
+-- Server version: 10.4.28-MariaDB
+-- PHP Version: 8.0.28
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+START TRANSACTION;
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `dbprofilein`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `analytics`
+--
+
+CREATE TABLE `analytics` (
+  `Analytics_ID` int(11) NOT NULL,
+  `Template_ID` int(11) NOT NULL,
+  `Views` int(11) NOT NULL,
+  `Downloads` int(11) NOT NULL,
+  `AvgSessionTime` float NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `certification`
+--
+
+CREATE TABLE `certification` (
+  `Certification_ID` int(11) NOT NULL,
+  `User_ID` int(11) NOT NULL,
+  `Name` varchar(100) NOT NULL,
+  `Institution` varchar(255) NOT NULL,
+  `Description` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `education`
+--
+
+CREATE TABLE `education` (
+  `Education_ID` int(11) NOT NULL,
+  `User_ID` int(11) NOT NULL,
+  `Degree_Name` varchar(100) NOT NULL,
+  `Description` text NOT NULL,
+  `Completion_Year` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `feedback`
+--
+
+CREATE TABLE `feedback` (
+  `Feedback_ID` int(11) NOT NULL,
+  `User_ID` int(11) NOT NULL,
+  `Content` text NOT NULL,
+  `DateSubmitted` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `personalinfo`
+--
+
+CREATE TABLE `personalinfo` (
+  `PersonalInfo_ID` int(11) NOT NULL,
+  `User_ID` int(11) NOT NULL,
+  `Name` varchar(100) NOT NULL,
+  `Profession` varchar(100) NOT NULL,
+  `Tagline` text NOT NULL,
+  `Email` varchar(50) NOT NULL,
+  `Phone` varchar(20) NOT NULL,
+  `Location` varchar(255) NOT NULL,
+  `ProfilePic` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `portfolio`
+--
+
+CREATE TABLE `portfolio` (
+  `Portfolio_ID` int(11) NOT NULL,
+  `User_ID` int(11) NOT NULL,
+  `Template_ID` int(11) NOT NULL,
+  `Title` varchar(100) NOT NULL,
+  `Content` text NOT NULL,
+  `Created_At` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `project`
+--
+
+CREATE TABLE `project` (
+  `Project_ID` int(11) NOT NULL,
+  `User_ID` int(11) NOT NULL,
+  `Name` varchar(100) NOT NULL,
+  `Description` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `skill`
+--
+
+CREATE TABLE `skill` (
+  `Skill_ID` int(11) NOT NULL,
+  `User_ID` int(11) NOT NULL,
+  `Name` varchar(100) NOT NULL,
+  `Description` text NOT NULL,
+  `Experience` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `template`
+--
+
+CREATE TABLE `template` (
+  `Template_ID` int(11) NOT NULL,
+  `Title` varchar(50) NOT NULL,
+  `Category` varchar(50) NOT NULL,
+  `Feature1` varchar(255) DEFAULT NULL,
+  `Feature2` varchar(255) DEFAULT NULL,
+  `Feature3` varchar(255) DEFAULT NULL,
+  `Image` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `template`
+--
+
+INSERT INTO `template` (`Template_ID`, `Title`, `Category`, `Feature1`, `Feature2`, `Feature3`, `Image`) VALUES
+(1, 'DevFolio', 'Developer', 'Github Integration', 'Skills Section', 'Projects', 'https://images.unsplash.com/photo-1517694712202-14dd9538aa97?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80'),
+(2, 'Photography', 'Photographer', 'Masonry Grid', 'Image Gallery', 'Categories', 'https://images.unsplash.com/photo-1452587925148-ce544e77e70d?ixlib=rb-1.2.1&auto=format&fit=crop&w=800&q=80');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user`
+--
+
+CREATE TABLE `user` (
+  `User_ID` int(11) NOT NULL,
+  `Name` varchar(50) NOT NULL,
+  `Email` varchar(100) NOT NULL,
+  `Password` varchar(255) NOT NULL,
+  `Created_At` timestamp NOT NULL DEFAULT current_timestamp()
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `user`
+--
+
+INSERT INTO `user` (`User_ID`, `Name`, `Email`, `Password`, `Created_At`) VALUES
+(1, 'M Shoaib', 'mshoaibarid@gmail.com', '$2y$10$lPZfPjBOM2YlNq/v.CBWA.y5E85IlcRm7G68ux8HyiCx08vrAIL2y', '2025-05-19 05:04:05');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `analytics`
+--
+ALTER TABLE `analytics`
+  ADD PRIMARY KEY (`Analytics_ID`),
+  ADD KEY `Template_ID` (`Template_ID`);
+
+--
+-- Indexes for table `certification`
+--
+ALTER TABLE `certification`
+  ADD PRIMARY KEY (`Certification_ID`),
+  ADD KEY `User_ID` (`User_ID`);
+
+--
+-- Indexes for table `education`
+--
+ALTER TABLE `education`
+  ADD PRIMARY KEY (`Education_ID`),
+  ADD KEY `User_ID` (`User_ID`);
+
+--
+-- Indexes for table `feedback`
+--
+ALTER TABLE `feedback`
+  ADD PRIMARY KEY (`Feedback_ID`),
+  ADD KEY `User_ID` (`User_ID`);
+
+--
+-- Indexes for table `personalinfo`
+--
+ALTER TABLE `personalinfo`
+  ADD PRIMARY KEY (`PersonalInfo_ID`),
+  ADD KEY `User_ID` (`User_ID`);
+
+--
+-- Indexes for table `portfolio`
+--
+ALTER TABLE `portfolio`
+  ADD PRIMARY KEY (`Portfolio_ID`),
+  ADD KEY `User_ID` (`User_ID`),
+  ADD KEY `Template_ID` (`Template_ID`);
+
+--
+-- Indexes for table `project`
+--
+ALTER TABLE `project`
+  ADD PRIMARY KEY (`Project_ID`),
+  ADD KEY `User_ID` (`User_ID`);
+
+--
+-- Indexes for table `skill`
+--
+ALTER TABLE `skill`
+  ADD PRIMARY KEY (`Skill_ID`),
+  ADD KEY `User_ID` (`User_ID`);
+
+--
+-- Indexes for table `template`
+--
+ALTER TABLE `template`
+  ADD PRIMARY KEY (`Template_ID`),
+  ADD UNIQUE KEY `Title` (`Title`);
+
+--
+-- Indexes for table `user`
+--
+ALTER TABLE `user`
+  ADD PRIMARY KEY (`User_ID`),
+  ADD UNIQUE KEY `Email` (`Email`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `analytics`
+--
+ALTER TABLE `analytics`
+  MODIFY `Analytics_ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `certification`
+--
+ALTER TABLE `certification`
+  MODIFY `Certification_ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `education`
+--
+ALTER TABLE `education`
+  MODIFY `Education_ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `feedback`
+--
+ALTER TABLE `feedback`
+  MODIFY `Feedback_ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `personalinfo`
+--
+ALTER TABLE `personalinfo`
+  MODIFY `PersonalInfo_ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `portfolio`
+--
+ALTER TABLE `portfolio`
+  MODIFY `Portfolio_ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `project`
+--
+ALTER TABLE `project`
+  MODIFY `Project_ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `skill`
+--
+ALTER TABLE `skill`
+  MODIFY `Skill_ID` int(11) NOT NULL AUTO_INCREMENT;
+
+--
+-- AUTO_INCREMENT for table `template`
+--
+ALTER TABLE `template`
+  MODIFY `Template_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
+
+--
+-- AUTO_INCREMENT for table `user`
+--
+ALTER TABLE `user`
+  MODIFY `User_ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `analytics`
+--
+ALTER TABLE `analytics`
+  ADD CONSTRAINT `analytics_ibfk_1` FOREIGN KEY (`Template_ID`) REFERENCES `template` (`Template_ID`);
+
+--
+-- Constraints for table `certification`
+--
+ALTER TABLE `certification`
+  ADD CONSTRAINT `certification_fk_user` FOREIGN KEY (`User_ID`) REFERENCES `user` (`User_ID`);
+
+--
+-- Constraints for table `education`
+--
+ALTER TABLE `education`
+  ADD CONSTRAINT `education_fk_user` FOREIGN KEY (`User_ID`) REFERENCES `user` (`User_ID`);
+
+--
+-- Constraints for table `feedback`
+--
+ALTER TABLE `feedback`
+  ADD CONSTRAINT `feedback_ibfk_1` FOREIGN KEY (`User_ID`) REFERENCES `user` (`User_ID`);
+
+--
+-- Constraints for table `personalinfo`
+--
+ALTER TABLE `personalinfo`
+  ADD CONSTRAINT `personalinfo_fk_user` FOREIGN KEY (`User_ID`) REFERENCES `user` (`User_ID`);
+
+--
+-- Constraints for table `portfolio`
+--
+ALTER TABLE `portfolio`
+  ADD CONSTRAINT `portfolio_ibfk_1` FOREIGN KEY (`Template_ID`) REFERENCES `template` (`Template_ID`);
+
+--
+-- Constraints for table `project`
+--
+ALTER TABLE `project`
+  ADD CONSTRAINT `project_fk_user` FOREIGN KEY (`User_ID`) REFERENCES `user` (`User_ID`);
+
+--
+-- Constraints for table `skill`
+--
+ALTER TABLE `skill`
+  ADD CONSTRAINT `skill_fk_user` FOREIGN KEY (`User_ID`) REFERENCES `user` (`User_ID`);
+COMMIT;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
